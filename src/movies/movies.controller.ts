bbc72ab823bc35a706744ffd19c3ec666cd875a9
windopper <open0916@naver.com>
2022-04-02 15:20:37 +0900

    done

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Res } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity.';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -20,26 +22,23 @@ export class MoviesController {
     }
 
     @Get("/:id")
-    getOne(@Param("id") id: string): Movie {
+    getOne(@Param("id") id: number): Movie {
         return this.moviesService.getOne(id)
     }
 
     @Post()
-    create(@Body() movieData) {
+    create(@Body() movieData: CreateMovieDto) {
         return this.moviesService.create(movieData)
     }
 
     @Delete("/:id") 
-    remove(@Param("id") movieId: string) {
+    remove(@Param("id") movieId: number) {
         return this.moviesService.deleteOne(movieId)
     }
 
     @Patch('/:id')
-    patch(@Param("id") movieId: string, @Body() updateData) {
-        return {
-            updateMovie: movieId,
-            ...updateData,
-        }
+    patch(@Param("id") movieId: number, @Body() updateData: UpdateMovieDto) {
+        return this.moviesService.update(movieId, updateData)
     }
 
 }
--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -15,4 +16,88 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe("getAll()", () => {
+    it("should return an array", () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne()', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'TEST',
+        year: 3434,
+        genres:['test']
+      })
+      const result = service.getOne(1);
+      expect(result).toBeDefined();
+      expect(result.id).toEqual(1)
+    })
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999)
+      }catch(e) {
+        expect(e).toBeInstanceOf(NotFoundException)
+        expect(e.message).toEqual('Movie with ID 999 not found.')
+      }
+    })
+  })
+
+  describe("deleteOne", () => {
+    it('deletes a movie', () => {
+      service.create({
+        title: 'Testing',
+        genres: ['test'],
+        year: 2000
+      })
+      const allMovies = service.getAll().length
+      service.deleteOne(1)
+      const afterDelete = service.getAll().length
+
+      expect(afterDelete).toBeLessThan(allMovies)
+    })
+    it("should return a 404", () => {
+      try {
+        service.getOne(999)
+      }catch(e) {
+        expect(e).toBeInstanceOf(NotFoundException)
+      }
+    })
+  })
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length
+      service.create({
+        title: 'Testing',
+        genres: ['test'],
+        year: 2000
+      })
+      const afterCreate = service.getAll().length
+      expect(afterCreate).toBeGreaterThan(beforeCreate)
+    })
+  })
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        title: 'Testing',
+        genres: ['test'],
+        year: 2000
+      })
+      service.update(1, {title:'update test'});
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('update test')
+    })
+
+    it('should throw a NotFouncException', () => {
+      try {
+        service.update(999, {})
+      }catch(e) {
+        expect(e).toBeInstanceOf(NotFoundException)
+      }
+    })
+  })
 });
